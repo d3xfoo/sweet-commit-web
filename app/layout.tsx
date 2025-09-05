cb5b15026a3186c1faa7c9e9ac99f357ae8e57d9
d3xfoo <thestraybyte@gmail.com>
2025-09-05 07:36:50 +0530

    feat(analytics): add page analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/next"
 import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
@@ -76,6 +77,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+        <Analytics/>
         </ThemeProvider>
       </body>
     </html>
